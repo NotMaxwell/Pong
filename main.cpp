9cd6eaab10623bc1bc54fda10626d6c6ad45ba71
NotMaxwell <William.mccormick@ascte.org>
2024-11-10 15:43:55 -0600

    Created base for game

--- main.cpp
+++ main.cpp
@@ -5,12 +5,48 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+
 int main(int argc, char* argv[]) {
     //test SDL
-    SDL_Init(SDL_INIT_EVERYTHING);
-    TTF_Init();
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Pong", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    //Game logic
+    bool running = true;
+
+    while(running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                running = false;
+            }
+            else if(event.type == SDL_KEYDOWN) {
+                if(event.key.keysym.sym == SDLK_ESCAPE){
+                    running = false;
+                }
+            }
+        }
+
+        //clear window
+        SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
+        SDL_RenderClear(renderer);
+
+        //rendereing
+
+
+        //Present back buffer
+        SDL_RenderPresent(renderer);
+    }
+    //Cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    TTF_Quit();
 
     return 0;
 }
+
+
